c15d848f08a8e03c6bfe4f4e34feac4a4b63ca09
Milan Sony <milansonyofficial@gmail.com>
2024-07-05 02:05:37 +0530

    user data displayed on console

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+router.post('/', (req, res) =>{
+    console.log(req.body)
+})
 
 
-
-
-// router.get('/', (req, res)=>{
-//     res.json({
-//         status: 200,
-//         message: 'users'
-//     })
-// })
+router.get('/', (req, res)=>{
+    res.json({
+        status: 200,
+        message: 'users data'
+    })
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const app = express();
 const db = require('./config/db');
 db.connect();
 
+// body parser
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 // Base URL (/)
 const routes = require('./routes')
 app.use('/', routes);
