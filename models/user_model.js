7bb0d818b602336ef2771f6ccc55000f3b8f8e1a
Milan Sony <milansonyofficial@gmail.com>
2024-07-05 01:43:09 +0530

    users schema created

---
+++ models/user_model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose'); 
+import { nanoid } from 'nanoid'
+
+
+const usersSchema = new mongoose.Schema({
+    user_id:{
+        type: String,
+        required: true,
+        unique: true,
+        default: nanoid()
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('users', usersSchema);
