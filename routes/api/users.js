fba7ce47d529c6ef35ce7d95383b0bd470442709
Milan Sony <milansonyofficial@gmail.com>
2024-07-06 16:54:33 +0530

    modified

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,8 +6,8 @@ const Users = require('../../models/user_model')
 const verifyToken = require('../../middlewares/authentication')
 
 router.get('/', verifyToken, async (req, res) => {
-    console.log(req.body)
-    console.log(req.user.user_id)
+    // console.log(req.body)
+    // console.log(req.user.user_id)
     let user = await Users.findOne({ user_id: req.user.user_id })
         .catch(err => {
             res.send(400).json({
