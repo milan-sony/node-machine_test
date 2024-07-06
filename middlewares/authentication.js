e2b419519ff568acd5cfb0ba20841f0101a33d50
Milan Sony <milansonyofficial@gmail.com>
2024-07-06 16:11:09 +0530

    user authentication file

---
+++ middlewares/authentication.js
@@ -0,0 +1,34 @@
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+const verifyToken = (req, res, next) => {
+    const token = req.header('Authorization'); //req.headers.authorization
+    if (!token) {
+        return res.status(401).json({
+            status: 401,
+            message: 'Unauthorized',
+            error: 'Access denied'
+        });
+    }
+
+    // split the bearer token "bearer efhjkhfjkhjkhsjkhsjkhjgkhkjsh"
+    const bearer = token.split(' ');
+    const bearerToken = bearer[1];
+
+    try {
+        const decoded = jwt.verify(bearerToken, process.env.TOKEN_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(400).json({
+            status: 400,
+            message: 'Invalid token',
+            error: error
+        });
+    }
+}
+
+
+module.exports = verifyToken;
