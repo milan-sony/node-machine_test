7d51fcedd5e97bdcd9af56769dda349e38f0748e
Milan Sony <milansonyofficial@gmail.com>
2024-07-06 16:09:51 +0530

    user schema modified

--- models/user_model.js
+++ models/user_model.js
@@ -1,28 +1,64 @@
 const mongoose = require('mongoose');
 const { nanoid } = require('nanoid');
 
-
-const usersSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     user_id: {
         type: String,
         required: true,
         unique: true,
         default: nanoid()
     },
-    username: {
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    mobile: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /\d{10}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /\S+@\S+\.\S+/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        }
+    },
+    role: {
+        type: String,
+        required: true,
+        default: 'user',
+        // list possiable values
+        enum: ['user', 'admin']
     },
     password: {
         type: String,
-        required: true
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        required: false,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        required: false,
+        default: Date.now
     }
 })
 
-module.exports = mongoose.model('users', usersSchema);
+module.exports = mongoose.model('users', userSchema);
