99cdb0ae53123c76b98bce3122cce9a5e05fa2cf
Milan Sony <milansonyofficial@gmail.com>
2024-07-08 10:57:55 +0530

    file upload

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -13,6 +13,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + '_' + Date.now() + '.csv')
+        console.log(Date.now())
     }
 });
 
